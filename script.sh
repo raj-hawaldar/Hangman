4ade397da4d9c1469a1c3beb6ed593993578b830
raj-hawaldar <rajhawaldar007@gmail.com>
2018-10-08 08:03:25 +0530

    Use this file as Final Script

--- script.sh
+++ script.sh
@@ -5,11 +5,15 @@ do
 	echo "commit id:$c"
 	parent_id=$(git log --pretty=%P -n 1 $c)
 	echo "Parent is $parent_id"
-	list=$(git diff $parent_id..$c --name-only --diff-filter=ACMRTUXB)
-	echo "Length is ${#list[@]}"
-	git archive -o update.zip $c $(git diff $parent_id..$c --name-only --diff-filter=ACMRTUXB)
-	for x in update.zip ; do unzip -d patch -o -u $x ; done
+	if [[ $(git diff $parent_id..$c --name-only --diff-filter=ACMRTUXB) ]]; then
+    	    echo "there are files"
+	    git archive -o update.zip $c $(git diff $parent_id..$c --name-only --diff-filter=ACMRTUXB)
+	    for x in update.zip ; do unzip -d patch -o -u $x ; done
+	else
+	    echo "no files found"
+	fi	
 	for f in $(git diff $parent_id..$c --name-only --diff-filter=D) ; do 
+		echo "patch/$f"
 		rm patch/$f
 	done
 done
