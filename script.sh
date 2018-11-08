1b1037911830195931c7c3e714bb94bf36564edb
raj-hawaldar <rajhawaldar007@gmail.com>
2018-11-08 19:44:57 +0530

    Revert "Use this file as Final Script"
    This reverts commit 4ade397da4d9c1469a1c3beb6ed593993578b830.

--- script.sh
+++ script.sh
@@ -5,15 +5,11 @@ do
 	echo "commit id:$c"
 	parent_id=$(git log --pretty=%P -n 1 $c)
 	echo "Parent is $parent_id"
-	if [[ $(git diff $parent_id..$c --name-only --diff-filter=ACMRTUXB) ]]; then
-    	    echo "there are files"
-	    git archive -o update.zip $c $(git diff $parent_id..$c --name-only --diff-filter=ACMRTUXB)
-	    for x in update.zip ; do unzip -d patch -o -u $x ; done
-	else
-	    echo "no files found"
-	fi	
+	list=$(git diff $parent_id..$c --name-only --diff-filter=ACMRTUXB)
+	echo "Length is ${#list[@]}"
+	git archive -o update.zip $c $(git diff $parent_id..$c --name-only --diff-filter=ACMRTUXB)
+	for x in update.zip ; do unzip -d patch -o -u $x ; done
 	for f in $(git diff $parent_id..$c --name-only --diff-filter=D) ; do 
-		echo "patch/$f"
 		rm patch/$f
 	done
 done
